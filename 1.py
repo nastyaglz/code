4baa08bd8b41d500c1486741d7d94f7177be2a55
nastyaglz <nastya.glazkova2008@gmail.com>
2024-10-16 18:25:39 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,30 @@
+'''a = int(input())
+if a > 14:
+    print('Добро пожаловать!')
+elif a < 14:
+    print('Вам еще рано')
+elif a == 14:
+    print('Вам 14 лет!Поздравляю!')'''
+
+'''a = input()
+if a == 'secret':
+    print("Доступ разрешен!")
+else:
+    print("Неверный пароль!")'''
+
+'''a = input()
+if a.isdigit():
+    print("Это цифра!")
+elif a.isalpha():
+    print("Это буква!")
+else:
+    print("Это не цифра и не буква!")'''
+
+
+'''a = int(input())
+if a > 1000:
+    print(a - (a * 0.1))
+elif a > 500:
+    print(a - (a * 0.05))
+else:
+    print(a)'''
